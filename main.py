458751ce1b6e12340d5525b6d287df42ea2e7a90
Dilip <sahud048@gmail.com>
2020-11-22 14:20:56 +0530

    Quates file added and made method to read files

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager,Screen
-import json
+import json,glob
 from datetime import datetime
+from pathlib import Path
 
 Builder.load_file("design.kv")
 
@@ -42,6 +43,15 @@ class LoginScreenSuccess(Screen):
     def log_out(self):
         self.manager.transition.direction = "right"
         self.manager.current = "login_screen"
+
+    def get_quote(self,feel):
+        feel = feel.lower()
+        available_feelings = glob.glob("quotes/*txt")
+        available_feelings = [ Path(feelings).stem for feelings in available_feelings]
+        if feel in available_feelings:
+            with open(f"quotes/{feel}.txt") as file:
+                quotes = file.readlines()
+            print(quotes)
 
 class MainApp(App):
     def build(self):
